c62ca2078923adadaeff5a9c786b9bef07409a06
Rahulpersie66 <Rahulpersie66@gmail.com>
2024-01-22 10:54:17 -0500

    Timeline added for job Timeline

--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -5,7 +5,7 @@ import java from '../../assests/java.png';
 import ui from '../../assests/ui.jpg';
 import 'primeicons/primeicons.css';
 import Timeline from '../Timeline/timeline';
-        
+import JobTimeline from '../Timeline/jobTimeline';
 
 const Skills = () => {
    const [isVisible, setIsVisible] = useState(false);
@@ -25,125 +25,35 @@ const toggleVisiblity3 = () => {
     setIsVisible3(!isVisible3);
 }
 
-// const educationTimeline = 
-// <div className='timelineBox'>
-//     <div className='timeline'>
-//         <div className='container left-container'>
-//             <div className='text-box'>
-//                 <h2 className='masterUnivName'>Binghamton University, Thomas J. Watson College of Engineering and Applied Science</h2>
-//                 <span className='masterDegreeDetails'>Master of Science in Computer Science</span>
-//                 <div className='datePass'>
-//                     <i className='pi pi-calendar'></i>
-//                     <p className='masterDuration'>Aug 2023 - May 2025</p>
-//                 </div>
-//             </div>
-//             <i className='pi pi-circle-fill circle-left'></i>
-//         </div>
-//     </div>
+const [showEducationTimeline, setShowEducationTimeline] = useState(true);
+const [showJobTimeline, setShowJobTimeline] = useState(false);
 
-//     <div className='timeline'>
-//         <div className='container right-container'>
-//         <i className='pi pi-circle-fill circle-right'></i>
-//             <div className='text-box'>
-//                 <h2 className='masterUnivName'>Jaypee University Of Information, Technology</h2>
-//                 <span className='masterDegreeDetails'>Bachelors of Technology in Computer Science Engineering</span>
-//                 <div className='datePass'>
-//                     <i className='pi pi-calendar'></i>
-//                     <p className='masterDuration'>Aug 2016 - Jul 2020</p>
-//                 </div>
-//             </div>
-//         </div>
-//     </div>
+const toggleEducationTimeline = () => {
+    // Close other timelines if needed
+    setShowJobTimeline(false);
 
-//     <div className='timeline'>
-//         <div className='container left-container'>
-//             <div className='text-box'>
-//                 <h2 className='masterUnivName'>Kendriya Vidyalaya Hamirpur</h2>
-//                 <span className='masterDegreeDetails'>Secondary School</span>
-//                 <div className='datePass'>
-//                     <i className='pi pi-calendar'></i>
-//                     <p className='masterDuration'>Apr 2013 - May 2015</p>
-//                 </div>
-//             </div>
-//             <i className='pi pi-circle-fill circle-left'></i>
-//         </div>
-//     </div>
+    setShowEducationTimeline(true);
 
-//     <div className='timeline'>
-//         <div className='container right-container'>
-//             <i className='pi pi-circle-fill circle-right'></i>
-//             <div className='text-box'>
-//                 <h2 className='masterUnivName'>Kendriya Vidyalaya Nadaun</h2>
-//                 <span className='masterDegreeDetails'>Primary School</span>
-//                 <div className='datePass'>
-//                     <i className='pi pi-calendar'></i>
-//                     <p className='masterDuration'>Apr 2003 - Mar 2013</p>
-//                 </div>
-//             </div>
-//         </div>
-//     </div>
+  };
 
-// </div>;
+  const toggleJobTimeline = () => {
+    // Close other timelines if needed
+    setShowEducationTimeline(false);
 
-// const jobTimeline = 
-// <div className='timelineBox'>
-//     <div className='timeline'>
-//         <div className='container left-container'>
-//             <div className='text-box'>
-//                 <h2 className='masterUnivName'>Nablasol Digital Solution Pvt. Ltd.</h2>
-//                 <span className='masterDegreeDetails'>Software Development Intern</span>
-//                 <div className='datePass'>
-//                     <i className='pi pi-calendar'></i>
-//                     <p className='masterDuration'>Dec 2022 - Mar 2023</p>
-//                 </div>
-//             </div>
-//             <i className='pi pi-circle-fill circle-left'></i>
-//         </div>
-//     </div>
+    setShowJobTimeline(true);
+  };
 
-//     <div className='timeline'>
-//         <div className='container right-container'>
-//         <i className='pi pi-circle-fill circle-right'></i>
-//             <div className='text-box'>
-//                 <h2 className='masterUnivName'>Clerisy Solution Pvt. Ltd.</h2>
-//                 <span className='masterDegreeDetails'>Full Stack Trainee</span>
-//                 <div className='datePass'>
-//                     <i className='pi pi-calendar'></i>
-//                     <p className='masterDuration'>Aug 2022 - Dec 2022</p>
-//                 </div>
-//             </div>
-//         </div>
-//     </div>
+const educationTimeline = 
+<Timeline className='timelineReact'/>;
+const jobTimeline =<JobTimeline/>;
 
-//     <div className='timeline'>
-//         <div className='container left-container'>
-//             <div className='text-box'>
-//                 <h2 className='masterUnivName'>SimpliLearn</h2>
-//                 <span className='masterDegreeDetails'>Full Stack certfication</span>
-//                 <div className='datePass'>
-//                     <i className='pi pi-calendar'></i>
-//                     <p className='masterDuration'>Jan 2022 - Jun 2022</p>
-//                 </div>
-//             </div>
-//             <i className='pi pi-circle-fill circle-left'></i>
-//         </div>
-//     </div>
 
-//     <div className='timeline'>
-//         <div className='container right-container'>
-//             <i className='pi pi-circle-fill circle-right'></i>
-//             <div className='text-box'>
-//                 <h2 className='masterUnivName'>Think Next Pvt. Ltd.</h2>
-//                 <span className='masterDegreeDetails'>AI/ML Intern</span>
-//                 <div className='datePass'>
-//                     <i className='pi pi-calendar'></i>
-//                     <p className='masterDuration'>May 2019 - Jun 2019</p>
-//                 </div>
-//             </div>
-//         </div>
-//     </div>
+// const [showEducationTimeline, setShowEducationTimeline] = useState(false);
 
-// </div>;
+// const educationTimelineFun = () => {
+//       // Toggle the state to show/hide education timeline
+//       setShowEducationTimeline(!showEducationTimeline);
+//     };
 
     return (
         <section id='skills'>
@@ -397,10 +307,10 @@ const toggleVisiblity3 = () => {
                 <span className='qualifiactionHeaderSpan'>My personal journey</span>
             </div>
             <div className='qualStudJob'>
-                <div className='eduactionQual'>
+                <div className='eduactionQual' onClick={toggleEducationTimeline} >
                     <i className='pi pi-book'><span className='educationQualHeader'>&nbsp;Education</span></i>  
                 </div>
-                <div className='jobQual'>
+                <div className='jobQual'  onClick={toggleJobTimeline}>
                     <i className='pi pi-briefcase'><span className='jobQualHeader'>&nbsp;Work</span></i>
                 </div>   
             </div>
@@ -408,8 +318,8 @@ const toggleVisiblity3 = () => {
         <br></br>
         
 
-        <Timeline className='timelineReact'/>
-
+      {showEducationTimeline && educationTimeline}
+      {showJobTimeline && jobTimeline}
 
             <div className='skillBars'>
                 {/* Skill Bar 1 */}
@@ -443,4 +353,4 @@ const toggleVisiblity3 = () => {
     );
 }
 
-export default Skills
+export default Skills;
---
+++ src/components/Timeline/jobTimeline.js
@@ -0,0 +1,49 @@
+import './jobTimeline.css'
+import React from 'react'
+import {VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css'
+import {FaGraduationCap} from 'react-icons/fa'
+
+const JobTimeline = () =>{
+    return(
+        <div className='experience'>
+            <VerticalTimeline lineColor='#247' className='verticalline'>
+                <VerticalTimelineElement className='vertical-timeline-element'
+                iconStyle={{background:'rgb(0,0,0)', color:'#fff'}} 
+                icon={<FaGraduationCap/>}
+                date = 'Dec 2022 - Mar 2023'
+                >
+                <h3 className='headTimeline'>Nablasol Digital Solution Pvt. Ltd.</h3>
+                <p className='paraTimeline'>Software Development Intern</p>
+                </VerticalTimelineElement>
+
+                <VerticalTimelineElement className='vertical-timeline-element'
+                iconStyle={{background:'rgb(0,0,0)', color:'#fff'}} 
+                icon={<FaGraduationCap/>}
+                date = 'Aug 2022 - Dec 2022'
+                >
+                <h3 className='headTimeline'>Clerisy Solution Pvt. Ltd.</h3>
+                <p className='paraTimeline'>Full Stack trainee</p>
+                </VerticalTimelineElement>
+                <VerticalTimelineElement className='vertical-timeline-element'
+                iconStyle={{background:'rgb(0,0,0)', color:'#fff'}} 
+                icon={<FaGraduationCap/>}
+                date = 'Jan 2022 - Jul 2022'
+                >
+                <h3 className='headTimeline'>Simplilearn Java Full Stack Online</h3>
+                <p className='paraTimeline'>Java Full Stack Student</p>
+                </VerticalTimelineElement>
+                <VerticalTimelineElement className='vertical-timeline-element'
+                iconStyle={{background:'rgb(0,0,0)', color:'#fff'}} 
+                icon={<FaGraduationCap/>}
+                date = 'Jun 2019 - Jul 2019'
+                >
+                <h3 className='headTimeline'>Think Next Pvt. Ltd.</h3>
+                <p className='paraTimeline'>AI/ML Student/Intern</p>
+                </VerticalTimelineElement>
+            </VerticalTimeline>
+        </div>
+    )
+}
+
+export default JobTimeline;
