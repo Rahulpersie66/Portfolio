75622cb77df3d2bf0b6a6cac915c98b0b4b310c1
Rahulpersie66 <Rahulpersie66@gmail.com>
2024-12-20 20:32:38 -0500

    checking dec 20

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -4,7 +4,7 @@ import './footer.css';
 const Footer = () => {
     return (
         <footer className='footer'>
-            Copyright &#169; 2024 Rahul Rana. All Rights recieved.
+            Copyright &#169; 2025 Rahul Rana. All Rights recieved.
         </footer>
     );
 }
